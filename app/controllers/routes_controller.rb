d0d17bb254d5baed09b5ac6dcea4c71f48c97e9a
sherri010 <aminshahidi.sh@gmail.com>
2016-11-19 18:36:15 -0800

    adds delete and update

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -16,16 +16,37 @@ class RoutesController < ApplicationController
   end
 
   def create
-    #  @cust.houses.create(params[:house])
-   p params
-   user = User.find(params[:user_id])
-   final = user.routes.create(:name => params[:name],:distance =>params[:distance])
-   if final
-     render :json =>{success:"OK"}, status: 200
-   else
-     render :json => {error:"FAILED"}, status: 400
+    #  route = current_user.routes.create(:name => params[:name],:distance =>params[:distance])
+      r = Route.create(route_params)
+      if r.valid?
+         render :json => r, status: 200
+      else
+         render :json => {error:'faild attempt'}, status: 400
+      end
   end
-end
+
+  def update
+     d = Route.find(params[:id]).destroy
+     c = Route.create(route_params)
+     p "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+     p c
+     p d
+     if c.valid? and d
+       render :json => c, status: 200
+    else
+       render :json => {error:'faild attempt'}, status: 400
+    end
+  end
+
+  def destroy
+     r = Route.find(params[:id]).destroy
+    if r
+       render :json => r, status: 200
+    else
+       render :json => {error:'faild attempt'}, status: 400
+    end
+  end
+
 private
   def route_params
     params.require(:route).permit(:name,:distance,:user_id)
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,4 +1,9 @@
 class Route < ApplicationRecord
   belongs_to :user
   has_many :nodes
+
+  validates :name, presence: true
+  validates :distance, presence: true
+  validates :user_id, presence: true
+
 end
