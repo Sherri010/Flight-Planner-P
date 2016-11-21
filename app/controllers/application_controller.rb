49495ae0c57e790c57ae73a44e30cad536d1e1db
sherri010 <aminshahidi.sh@gmail.com>
2016-11-21 10:28:11 -0800

    adds angular and views to the backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,17 +7,21 @@ class ApplicationController < ActionController::Base
   #           @auth_user = User.find_by(auth_token: token)
   #       end
   #   end
-   protect_from_forgery with: :null_session
+   protect_from_forgery with: :exception
 
-    def authenticate
-        authenticate_or_request_with_http_token do |token, options|
-            user = User.find_by(auth_token: token)
+    # def authenticate
+    #     authenticate_or_request_with_http_token do |token, options|
+    #         user = User.find_by(auth_token: token)
 
-            if user
-                return true
-            else
-                render :json => { error: "Invalid Token" }, status: 401
-            end
-        end
+    #         if user
+    #             return true
+    #         else
+    #             render :json => { error: "Invalid Token" }, status: 401
+    #         end
+    #     end
+    # end
+
+    def after_sign_in_path_for(resource)
+      "/dashboard"
     end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Users::SessionsController < Devise::SessionsController
-  respond_to :json
+  # respond_to :json
 # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
