fffba7fce0da33734d6e7998ea06f3ba2233ea41
sherri010 <aminshahidi.sh@gmail.com>
2016-11-21 14:11:16 -0800

    adds UR router

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,25 +1,100 @@
-var app = angular.module("flightApp", ["ngRoute"]);
+var app = angular.module("flightApp", ["ui.router"]);
 
-app.config(function($routeProvider) {
-		$routeProvider
-			// route to new plan
-			.when('/new-plan', {
-				templateUrl : 'pages/plan.html',
-				controller  : 'PlanController'
-			})
-      //route to weather
-			.when('/weather', {
-				templateUrl : 'pages/weather.html',
-				controller  : 'weatherController'
-			})
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state("newplan", {
+      url: "/new-plan",
+      views: {
+        "base": {
+          templateUrl: "pages/base.html"
+        },
+        "actions@newplan": {
+          templateUrl: "pages/plan.html",
+          controller: "PlanController"
+        }
+      }
+    })
+    .state("weather", {
+      url: "/weather",
+      views: {
+        "base": {
+          templateUrl: "pages/base.html"
+        },
+        "actions@weather": {
+          templateUrl: "pages/weather.html",
+          controller: "weatherController"
+        },
+        "map@weather": {
+          controller: "MapController",
+          templateUrl: "pages/map.html"
+        }
+      }
+    })
+    // $routeProvider
+		// 	// route to new plan
+		// 	.when('/new-plan', {
+		// 		templateUrl : 'pages/plan.html',
+		// 		controller  : 'PlanController'
+		// 	})
+    //   //route to weather
+		// 	.when('/weather', {
+		// 		templateUrl : 'pages/weather.html',
+		// 		controller  : 'weatherController'
+		// 	})
+    //
+		// 	// route to airport
+		// 	.when('/airports', {
+		// 		templateUrl : 'pages/airports.html',
+		// 		controller  : 'airportController'
+		// 	});
 
-			// route to airport
-			.when('/airports', {
-				templateUrl : 'pages/airports.html',
-				controller  : 'airportController'
-			});
+      $urlRouterProvider.otherwise("/new-plan");
 	});
 
+  app.controller('MapController', function() {
+    var vm = this;
+
+    var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var labelIndex = 0;
+
+    var marker_list=[];
+
+    function initMap() {
+      var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 9,
+        center: {lat: 37.712, lng: -122.213},
+        streetViewControl: false,
+        mapTypeId: 'terrain'
+      });
+
+
+      google.maps.event.addListener(map, 'click', function(event) {
+        var new_marker={lat:event.latLng.lat(),lng:event.latLng.lng() }
+        marker_list.push(new_marker);
+        console.log(marker_list)
+        addMarker(event.latLng, map);
+        var flightPath = new google.maps.Polyline({
+          path: marker_list,
+          geodesic: true,
+          strokeColor: '#ff0000',
+          strokeOpacity: 1.0,
+          strokeWeight: 4
+        });
+    flightPath.setMap(map);
+     });
+     function addMarker(location, map) {
+       var marker = new google.maps.Marker({
+         position: location,
+         label: labels[labelIndex++ % labels.length],
+         map: map
+       });
+
+     }
+
+    }
+
+    initMap();
+  });
 
 	app.controller('PlanController', function() {
     var vm= this;
--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,40 +1,45 @@
-var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var labelIndex = 0;
-
-
-function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 9,
-    center: {lat: 37.712, lng: -122.213},
-    streetViewControl: false,
-    mapTypeId: 'terrain'
-  });
-
-
-  google.maps.event.addListener(map, 'click', function(event) {
-   addMarker(event.latLng, map);
- });
- function addMarker(location, map) {
-   var marker = new google.maps.Marker({
-     position: location,
-     label: labels[labelIndex++ % labels.length],
-     map: map
-   });
-
- }
-}
-
-  // var flightPlanCoordinates = [
-  //   {lat: 37.712, lng: -122.213},
-  //   {lat: 38.283, lng: -122.288},
-  //   {lat: 38.044, lng: -122.906},
-  //   {lat: 37.618, lng: -122.384}
-  // ];
-  // var flightPath = new google.maps.Polyline({
-  //   path: flightPlanCoordinates,
-  //   geodesic: true,
-  //   strokeColor: '#ff0000',
-  //   strokeOpacity: 1.0,
-  //   strokeWeight: 4
-  // });
-  // flightPath.setMap(map);
+// var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// var labelIndex = 0;
+//
+// var marker_list=[];
+//
+// function initMap() {
+//   var map = new google.maps.Map(document.getElementById('map'), {
+//     zoom: 9,
+//     center: {lat: 37.712, lng: -122.213},
+//     streetViewControl: false,
+//     mapTypeId: 'terrain'
+//   });
+//
+//
+//   google.maps.event.addListener(map, 'click', function(event) {
+//     var new_marker={lat:event.latLng.lat(),lng:event.latLng.lng() }
+//     marker_list.push(new_marker);
+//     console.log(marker_list)
+//     addMarker(event.latLng, map);
+//     var flightPath = new google.maps.Polyline({
+//       path: marker_list,
+//       geodesic: true,
+//       strokeColor: '#ff0000',
+//       strokeOpacity: 1.0,
+//       strokeWeight: 4
+//     });
+// flightPath.setMap(map);
+//  });
+//  function addMarker(location, map) {
+//    var marker = new google.maps.Marker({
+//      position: location,
+//      label: labels[labelIndex++ % labels.length],
+//      map: map
+//    });
+//
+//  }
+//
+// }
+//
+//   // var flightPlanCoordinates = [
+//   //   {lat: 37.712, lng: -122.213},
+//   //   {lat: 38.283, lng: -122.288},
+//   //   {lat: 38.044, lng: -122.906},
+//   //   {lat: 37.618, lng: -122.384}
+//   // ];
