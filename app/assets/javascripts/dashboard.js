9f934808a015d5c90ad6c756041261faec1717b9
sherri010 <aminshahidi.sh@gmail.com>
2016-11-21 11:52:42 -0800

    adds angular controllers

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -1,1 +1,35 @@
 var app = angular.module("flightApp", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+		$routeProvider
+			// route to new plan
+			.when('/new-plan', {
+				templateUrl : 'pages/plan.html',
+				controller  : 'PlanController'
+			})
+      //route to weather
+			.when('/weather', {
+				templateUrl : 'pages/weather.html',
+				controller  : 'weatherController'
+			})
+
+			// route to airport
+			.when('/airports', {
+				templateUrl : 'pages/airports.html',
+				controller  : 'airportController'
+			});
+	});
+
+
+	app.controller('PlanController', function($scope) {
+
+		$scope.message = 'new plan stuff';
+	});
+
+  app.controller('weatherController', function($scope) {
+		$scope.message = 'weahter api data';
+	});
+
+app.controller('airportController',function($scope){
+  $scope.message = 'airport list api';
+});
--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,3 +1,6 @@
+var labels = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var labelIndex = 0;
+
 
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -7,19 +10,31 @@ function initMap() {
     mapTypeId: 'terrain'
   });
 
-  var flightPlanCoordinates = [
-    {lat: 37.712, lng: -122.213},
-    {lat: 38.283, lng: -122.288},
-    {lat: 38.044, lng: -122.906},
-    {lat: 37.618, lng: -122.384}
-  ];
-  var flightPath = new google.maps.Polyline({
-    path: flightPlanCoordinates,
-    geodesic: true,
-    strokeColor: '#ff0000',
-    strokeOpacity: 1.0,
-    strokeWeight: 4
-  });
 
-  flightPath.setMap(map);
+  google.maps.event.addListener(map, 'click', function(event) {
+   addMarker(event.latLng, map);
+ });
+ function addMarker(location, map) {
+   var marker = new google.maps.Marker({
+     position: location,
+     label: labels[labelIndex++ % labels.length],
+     map: map
+   });
+
+ }
 }
+
+  // var flightPlanCoordinates = [
+  //   {lat: 37.712, lng: -122.213},
+  //   {lat: 38.283, lng: -122.288},
+  //   {lat: 38.044, lng: -122.906},
+  //   {lat: 37.618, lng: -122.384}
+  // ];
+  // var flightPath = new google.maps.Polyline({
+  //   path: flightPlanCoordinates,
+  //   geodesic: true,
+  //   strokeColor: '#ff0000',
+  //   strokeOpacity: 1.0,
+  //   strokeWeight: 4
+  // });
+  // flightPath.setMap(map);
