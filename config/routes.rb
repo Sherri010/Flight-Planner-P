8d1cb6e68185248b8951e5f338aa17c29b2f163e
sherri010 <aminshahidi.sh@gmail.com>
2016-11-21 10:51:33 -0800

    fixes the root route- removes notice and alert p tags

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # root "users/sessions#new"
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users, :controllers => { registrations: "users/registrations", sessions: "users/sessions" }
-
+  root :to => redirect("/users/sign_in")
   delete "/routes/:id/nodes/clear", to: "routes#destroy_all_nodes"
 
   resources :routes do
